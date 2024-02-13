37c1dd9b7ea97f4cfc7f192345233e0af250fd90
aggoado <150601099+aggoado@users.noreply.github.com>
2024-02-12 23:44:41 -0300

    Update entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,20 +1,22 @@
+#!/bin/bash
+
 echo "##################################"
 echo "####### ENTRYPOINT.bash ##########"
 echo "##################################"
 
 # FIX https://stackoverflow.com/questions/50142049/enospc-no-space-left-on-device-nodejs
 echo "# fix: no space left on device"
-echo 524288 | tee /proc/sys/fs/inotify/max_user_watches
-sysctl -p /etc/sysctl.conf
+echo 524288 | sudo tee /proc/sys/fs/inotify/max_user_watches
+sudo sysctl -p /etc/sysctl.conf
 echo "# Max user watches:"
 cat /proc/sys/fs/inotify/max_user_watches
 
-
-# test deno
+# Test Deno
+echo "# Testing Deno version"
 deno --version
 
-#bash
-echo "# start vscode as $USER"
-whoami
-echo "# start vscode"
-su $USER -c '/usr/bin/code --verbose --user-data-dir /userdata'
+# Start VSCode
+echo "# Starting VSCode as $USER"
+echo "Current user: $(whoami)"
+echo "# Starting VSCode"
+sudo -u $USER /usr/bin/code --verbose --user-data-dir /userdata
